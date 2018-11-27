64efdec0debe1862c61c9180e2388fd601933427
Sean Phelan <seanphelan97@gmail.com>
2018-11-27 09:58:48 -0700

    update user for question query

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
 const userSchema = new mongoose.Schema({
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true, minlength: 10},
   firstName: {type: String},
-  lastName: {type: String}
+  lastName: {type: String},
+  rootQuestion: {type: ObjectId}
 });
 
 userSchema.virtual('fullname').get(function() {
@@ -16,6 +19,7 @@ userSchema.set('toObject', {
   versionKey: false,
   transform: function(doc, result) {
     delete result.password;
+    delete result.rootQuestion;
     delete result._id;
     return result;
   }
